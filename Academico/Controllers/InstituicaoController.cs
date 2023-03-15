6a8d0b3f58d1d8ff8b68eb069366d05d706c15ee
Tamires Paiva <tamires.paiva@outlook.com>
2023-03-14 21:11:16 -0300

    Controle de Insitutições com Listagem e Gravação Funcionando

---
+++ Academico/Controllers/InstituicaoController.cs
@@ -0,0 +1,39 @@
+﻿using Academico.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Academico.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        private static IList<Instituicao> instituicoes = new List<Instituicao>()
+        {
+            new Instituicao
+            {
+                Id=1,
+                Nome = "Hogwarts",
+                Endereco = "Escócia"
+            },
+            new Instituicao
+            {
+                Id=2,
+                Nome = "Mansão X",
+                Endereco = "Nova Iorque"
+            }
+        };
+        public IActionResult Index()
+        {
+            return View(instituicoes);
+        }
+        public IActionResult Create()
+        { return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Instituicao instituicao)
+        {
+            instituicao.Id = instituicoes.Select(i => i.Id).Max() + 1;
+            instituicoes.Add(instituicao);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Academico/Models/Instituicao.cs
@@ -0,0 +1,9 @@
+﻿namespace Academico.Models
+{
+    public class Instituicao
+    {
+        public long? Id { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+    }
+}
